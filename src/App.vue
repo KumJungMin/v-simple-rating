60310ffb3ea94acabc458b6d72a6e83ddfe1a6cc
KumJungMin <rmawjdals@naver.com>
2024-08-27 20:47:56 +0900

    feat: add Rating component example

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,22 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
+import { ref } from "vue";
+import FaceEmptyIcon from "./assets/icon/face.svg";
+import FaceFillIcon from "./assets/icon/face-fill.svg";
+
+import Rating from "./components/Rating.vue";
+
+const rating = ref(0);
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
-</template>
+  <Rating />
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
+  <hr />
+  <Rating
+    :fillIcon="FaceFillIcon"
+    :emptyIcon="FaceEmptyIcon"
+    v-model="rating"
+  />
+  <p>rating: {{ rating }}</p>
+  <button @click="rating = 3">change rating to 3</button>
+</template>
